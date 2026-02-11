aab09804fc89d8f5a22ea81e188ae834b9b0cb53
Rohan Coder <sablerohan125@gmail.com>
2026-02-11 20:10:16 +0530

    Updated

--- src/components/spinner.jsx
+++
@@ -1,7 +0,0 @@
-export default function Spinner() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-950">
-      <div className="w-10 h-10 border-4 border-gray-700 border-t-indigo-500 rounded-full animate-spin" />
-    </div>
-  );
-}
